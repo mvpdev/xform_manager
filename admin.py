97b3d9dbc3797bea38b5cc26931f728d66dd0062
Andrew Marder <andrew.ei.marder@gmail.com>
2011-02-08 15:32:44 -0500

    switched over to relative imports

--- admin.py
+++ admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from trkr.xform_manager.models.instance import Instance
-from trkr.xform_manager.models.attachment import Attachment
-from trkr.xform_manager.models.survey_type import SurveyType
-from trkr.xform_manager.models.xform import XForm
+from .models.instance import Instance
+from .models.attachment import Attachment
+from .models.survey_type import SurveyType
+from .models.xform import XForm
 
 admin.site.register(Instance)
 admin.site.register(Attachment)
