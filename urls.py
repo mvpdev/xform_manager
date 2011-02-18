455c7acd3d2eec68da88c5bac2ee8b0b34e44f79
Alex Dorey <dorey415@gmail.com>
2011-02-18 12:06:21 -0500

    updating urls from x/... to xform/...

--- urls.py
+++ urls.py
@@ -22,18 +22,18 @@ FORM_LIST = "form-list"
 #browser accessible urls
 #-general:
 #    /
-#    /x/new
-#    /x/show
-#    /x/hide
-#    /x/(xform_id) #edit
-#    /x/(xform_id).xml #download
+#    /xform/new
+#    /xform/show
+#    /xform/hide
+#    /xform/(xform_id) #edit
+#    /xform/(xform_id).xml #download
 #-group-specific:
 #    /group_name/
-#    /group_name/x/new
-#    /group_name/x/show
-#    /group_name/x/hide
-#    /group_name/x/(xform_id)
-#    /group_name/x/(xform_id).xml
+#    /group_name/xform/new
+#    /group_name/xform/show
+#    /group_name/xform/hide
+#    /group_name/xform/(xform_id)
+#    /group_name/xform/(xform_id).xml
 
 #For debugging with no groups: OPT_GROUP_REGEX = ""
 OPT_GROUP_REGEX = "((?P<group_name>[^/]+)/)?"
@@ -42,9 +42,9 @@ urlpatterns = patterns('',
     url(r"^%sformList$" % OPT_GROUP_REGEX, views.formList, name=FORM_LIST),
     url(r"^%ssubmission$" % OPT_GROUP_REGEX, views.submission),
 
-    url(r"^%sx/new/$" % OPT_GROUP_REGEX, views.create_xform),
-    url(r"^%sx/(?P<id>\S+)\.xml$" % OPT_GROUP_REGEX, views.download_xform, name=DOWNLOAD_XFORM),
-    url(r"^%sx/(?P<pk>\S+)/(?P<show_hide>(show|hide))$" % OPT_GROUP_REGEX, views.show_hide_xform),
-    url(r"^%sx/(?P<id>\S+)/$" % OPT_GROUP_REGEX, views.show_xform),
+    url(r"^%sxform/new/$" % OPT_GROUP_REGEX, views.create_xform),
+    url(r"^%sxform/(?P<id>\S+)\.xml$" % OPT_GROUP_REGEX, views.download_xform, name=DOWNLOAD_XFORM),
+    url(r"^%sxform/(?P<pk>\S+)/(?P<show_hide>(show|hide))$" % OPT_GROUP_REGEX, views.show_hide_xform),
+    url(r"^%sxform/(?P<id>\S+)/$" % OPT_GROUP_REGEX, views.show_xform),
     url(r"^%s$" % OPT_GROUP_REGEX, views.list_xforms, name=LIST_XFORMS),
 )
