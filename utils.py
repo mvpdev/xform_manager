7e500bbb0e465ee060d6f8784113452451dd1398
Andrew Marder <andrew.ei.marder@gmail.com>
2011-03-21 05:48:49 -0400

    Moved XFormParser out of xform_manager and into parsed_xforms. Also, improved exception reporting to print exceptions when we're in testing mode.

--- utils.py
+++ utils.py
@@ -132,81 +132,6 @@ def _all_attributes(node):
             yield pair
 
 
-# test = {"one/two" : 1, "one/three" : 3, "two" : 2}
-# vardict = VariableDictionary(test)
-# print vardict["one"]._d, vardict["two"]
-
-
-class XFormParser(object):
-    def __init__(self, xml):
-        assert type(xml)==str or type(xml)==unicode, u"xml must be a string"
-        self.doc = minidom.parseString(xml)
-        self.root_node = self.doc.documentElement
-
-    def get_variable_list(self):
-        """
-        Return a list of pairs [(path to variable1, attributes of variable1), ...].
-        """
-        bindings = self.doc.getElementsByTagName(u"bind")
-        attributes = [dict(_all_attributes(b)) for b in bindings]
-        # note: nodesets look like /water/source/blah we're returning source/blah
-        return [(SLASH.join(d.pop(u"nodeset").split(SLASH)[2:]), d) for d in attributes]
-
-    def get_variable_dictionary(self):
-        d = {}
-        for path, attributes in self.get_variable_list():
-            assert path not in d, u"Paths should be unique."
-            d[path] = attributes
-        return d
-
-    def follow(self, path):
-        """
-        Path is an array of node names. Starting at the document
-        element we follow the path, returning the final node in the
-        path.
-        """
-        element = self.doc.documentElement
-        count = {}
-        for name in path.split(SLASH):
-            count[name] = 0
-            for child in element.childNodes:
-                if isinstance(child, minidom.Element) and child.tagName==name:
-                    count[name] += 1
-                    element = child
-            assert count[name]==1
-        return element
-
-    def get_id_string(self):
-        """
-        Find the single child of h:head/model/instance and return the
-        attribute 'id'.
-        """
-        instance = self.follow(u"h:head/model/instance")
-        children = [child for child in instance.childNodes \
-                        if isinstance(child, minidom.Element)]
-        assert len(children)==1
-        return children[0].getAttribute(u"id")
-
-    def get_title(self):
-        title = self.follow(u"h:head/h:title")
-        assert len(title.childNodes)==1, u"There should be a single title"
-        return title.childNodes[0].nodeValue
-
-    supported_controls = ["input", "select1", "select", "upload"]
-
-    def get_control_dict(self):
-        def get_pairs(e):
-            result = []
-            if hasattr(e, "tagName") and e.tagName in self.supported_controls:
-                result.append( (e.getAttribute("ref"),
-                                get_text(follow(e, "label").childNodes)) )
-            if e.hasChildNodes:
-                for child in e.childNodes:
-                    result.extend(get_pairs(child))
-            return result
-        return dict(get_pairs(self.follow("h:body")))
-
-
 # f = open(sys.argv[1])
 # xform = XFormParser(f.read())
 # f.close()
@@ -222,7 +147,7 @@ def report_exception(subject, info, exc_info=None):
         info += u"Exception in request: %s: %s" % (cls.__name__, err)
         info += u"".join(traceback.format_exception(*exc_info))
 
-    if settings.DEBUG:
+    if settings.DEBUG or settings.TESTING_MODE:
         print subject
         print info
     else:
