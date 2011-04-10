ce5ecd7b79b77a99917f3d6a189d4e3bacd90187
Andrew Marder <andrew.ei.marder@gmail.com>
2011-04-10 19:41:27 -0400

    Removed the tag.py file, all tags are defined the root of the project in common_tags.py

--- tag.py
+++
@@ -1,8 +0,0 @@
-# these are common XForm submission tags that we'll want to access
-XFORM_ID_STRING = u"_id_string"
-INSTANCE_DOC_NAME = u"_name"
-DATE_TIME_START = u"start"
-DATE_TIME_END = u"end"
-IMEI = u"device_id"
-PICTURE = u"picture"
-GPS = u"geopoint"
